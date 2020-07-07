ac2bbcb10f1e0081f453e281ee42e669d3c3a09a
hbajwaa <bajwahassan3@outlook.com>
2020-07-07 19:04:13 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times do
-  Todo.create(task: 'Seeded data')
-end
+
+  Todo.create(task: 'task 1')
+  Todo.create(task: 'task 2')
+  Todo.create(task: 'task 3')
+  Todo.create(task: 'task 4')
+  Todo.create(task: 'task 5')
+  Todo.create(task: 'task 6')
+  Todo.create(task: 'task 7')
+  Todo.create(task: 'task 8')
+  Todo.create(task: 'task 9')
